f2d9d5083c421bbd1d50d64d9bf5ff0396b0d401
MD SAIFUL ISLAM <138612747+2SAIFUL7ISLAM3@users.noreply.github.com>
2023-09-13 07:03:55 +0600

    somthing dine

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -2,8 +2,8 @@ import Profile from '../images/profile.png';
 
 const Header = () => {
     return (
-        <div className='flex justify-around items-center mt-6 '>
-            <h1 className=" text-4xl text-black font-bold  ">
+        <div className='flex justify-between items-center mx-20 my-3  p-2 border-b-2 '>
+            <h1 className=" text-4xl text-orange-300 font-bold  ">
                 Knowledge Cafe
             </h1>
             <img src={Profile} alt="" />
